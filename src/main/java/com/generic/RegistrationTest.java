ea6d69b32170754ee7c133faa39da4bfef3808b2
studentqa2021 <sarowerhome@gmail.com>
2023-01-09 22:56:58 -0500

    reviewd registration feature

--- src/main/java/com/generic/RegistrationTest.java
+++ src/main/java/com/generic/RegistrationTest.java
@@ -2,6 +2,6 @@ package com.generic;
 
 public class RegistrationTest {
 	public static void main(String[] args) {
-		System.out.println("Validated Registration Feature!");
+		System.out.println("Validated & Tested Registration Feature!");
 	}
 }
